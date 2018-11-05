6c7d4225c308d610ea942813861ab8de7b4aa914
dreyevihor <dreyevihor@gmail.com>
2018-11-05 14:02:27 +0200

    Create populate.sql

---
+++ km-52/Dreiev_Ihor/workshop1/sql/populate.sql
@@ -0,0 +1,16 @@
+insert into "CUSTOMER" (PK_CUSTOMER, NAME, USERNAME, EMAIL, PASSWORD) VALUES 
+(1, "asd", "asd", "asd@gmail.com", "asd"),
+insert into "CUSTOMER" (PK_CUSTOMER, NAME, USERNAME, EMAIL, PASSWORD) VALUES (2, "asddd", "asddd", "asddd@gmail.com", "asddd"),
+insert into "CUSTOMER" (PK_CUSTOMER, NAME, USERNAME, EMAIL, PASSWORD) VALUES (3, "zzxcx", "zzxcx", "zzxcx@gmail.com", "zzxcx"),
+insert into "CUSTOMER" (PK_CUSTOMER, NAME, USERNAME, EMAIL, PASSWORD) VALUES (4, "azzzsd", "azzzsd", "azzzsd@gmail.com", "azzzsd")
+
+insert into "QUESTIONSET" (PK_QUESTIONSET, NAME, CUSTOMER) VALUES
+(1, "asd", 1),
+insert into "QUESTIONSET" (PK_QUESTIONSET, NAME, CUSTOMER) VALUES (2, "asddd", 2),
+insert into "QUESTIONSET" (PK_QUESTIONSET, NAME, CUSTOMER) VALUES (3, "zzxcx", 3),
+insert into "QUESTIONSET" (PK_QUESTIONSET, NAME, CUSTOMER) VALUES (4, "azzzsd", 4)
+
+insert into "QUESTIONSET" (PK_QUESTION, TEXT, QUESTION_SET) VALUES (1, "asd", 1),
+insert into "QUESTIONSET" (PK_QUESTION, TEXT, QUESTION_SET) VALUES (2, "asddd", 2),
+insert into "QUESTIONSET" (PK_QUESTION, TEXT, QUESTION_SET) VALUES (3, "zzxcx", 3),
+insert into "QUESTIONSET" (PK_QUESTION, TEXT, QUESTION_SET) VALUES (4, "azzzsd", 4)
